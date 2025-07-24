73bebbb8f2637cb3dac3696f47936a76d7caa6ad
slava <slava.minasyan2006@mail.ru>
2025-07-25 00:21:04 +0300

    feat: Rewrite rest controller for init user state in system

--- mg-identity-service-user-spring-rest-adapter/src/main/java/ru/solomka/identity/user/v1/UserSecurityRestController.java
+++ mg-identity-service-user-spring-rest-adapter/src/main/java/ru/solomka/identity/user/v1/UserSecurityRestController.java
@@ -1,24 +1,61 @@
 package ru.solomka.identity.user.v1;
 
+import lombok.AccessLevel;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.solomka.identity.authentication.cqrs.AuthenticationCommand;
+import ru.solomka.identity.authentication.cqrs.RegistrationCommand;
+import ru.solomka.identity.common.cqrs.CommandHandler;
+import ru.solomka.identity.token.TokenPair;
+import ru.solomka.identity.user.UserEntity;
+import ru.solomka.identity.user.request.AuthenticationRequest;
 import ru.solomka.identity.user.request.RegistrationRequest;
-import ru.solomka.identity.user.response.AuthenticationResponse;
+import ru.solomka.identity.user.response.security.AuthenticationResponse;
+import ru.solomka.identity.user.response.security.RegistrationResponse;
 
-@RestController("/v1/identity/security")
+@RestController
+@RequestMapping("/v1/security/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserSecurityRestController {
 
-    @PostMapping(value = "/authentication", produces = "application/json")
-    public ResponseEntity<AuthenticationResponse> authenticationUser(@RequestBody AuthenticationResponse authenticationResponse) {
-        // TODO: Principal service & Permissions
-        return ResponseEntity.ofNullable(null);
+    @NonNull CommandHandler<RegistrationCommand, UserEntity> registrationCommandHandler;
+    @NonNull CommandHandler<AuthenticationCommand, TokenPair> authenticationCommandHandler;
+
+    @PostMapping(value = "/signin", produces = "application/json")
+    public ResponseEntity<AuthenticationResponse> signinUser(@RequestBody AuthenticationRequest authenticationRequest) {
+        TokenPair tokenPair = authenticationCommandHandler.handle(new AuthenticationCommand(
+                authenticationRequest.getLogin(), authenticationRequest.getPassword()
+        ));
+        return ResponseEntity.ok(
+                AuthenticationResponse.builder()
+                .accessToken(tokenPair.getAccessToken().getToken())
+                .refreshToken(tokenPair.getRefreshToken().getToken())
+                .build()
+        );
     }
 
-    @PostMapping(value = "/registration", produces = "application/json")
-    public ResponseEntity<AuthenticationResponse> authenticationUser(@RequestBody RegistrationRequest authenticationResponse) {
-        // TODO: Principal service & Permissions
-        return ResponseEntity.ofNullable(null);
+    @PostMapping(value = "/signup", produces = "application/json")
+    public ResponseEntity<RegistrationResponse> signupUser(@RequestBody RegistrationRequest registrationRequest) {
+        UserEntity userEntity = registrationCommandHandler.handle(new RegistrationCommand(
+                registrationRequest.getLogin(),
+                registrationRequest.getPassword(),
+                registrationRequest.getEmail(),
+                registrationRequest.getFirstName(),
+                registrationRequest.getLastName())
+        );
+        return ResponseEntity.ok(RegistrationResponse.builder()
+                .login(userEntity.getLogin())
+                .email(userEntity.getEmail())
+                .firstName(userEntity.getFirstName())
+                .lastName(userEntity.getLastName())
+                .build()
+        );
     }
 }
