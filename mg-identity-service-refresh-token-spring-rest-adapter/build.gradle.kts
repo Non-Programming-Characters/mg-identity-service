04640be48a230c41b5acb10a4ce231e9ec2da117
slava <slava.minasyan2006@mail.ru>
2025-07-25 00:15:15 +0300

    feat: Added build.gradle.kts

--- mg-identity-service-refresh-token-spring-rest-adapter/build.gradle.kts
+++ mg-identity-service-refresh-token-spring-rest-adapter/build.gradle.kts
@@ -1,6 +1,8 @@
 dependencies {
 
     implementation(project(":mg-identity-service-token"))
+    implementation(project(":mg-identity-service-common"))
+    implementation(project(":mg-identity-service-refresh-token"))
 
     implementation(rootProject.libs.springframework.spring.boot.starter.web)
     implementation(rootProject.libs.springdoc.springdoc.openapi.starter.webmvc.ui)
--- mg-identity-service-user-spring-rest-adapter/build.gradle.kts
+++ mg-identity-service-user-spring-rest-adapter/build.gradle.kts
@@ -2,5 +2,10 @@ dependencies {
     implementation(rootProject.libs.springframework.spring.boot.starter.web)
 
     implementation(project(":mg-identity-service-common"))
+
     implementation(project(":mg-identity-service-user"))
+
+    implementation(project(":mg-identity-service-authentication"))
+
+    implementation(project(":mg-identity-service-token"))
 }
