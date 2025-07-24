6560e19f46f380ca72485e43ee8c9486a61b128b
slava <slava.minasyan2006@mail.ru>
2025-07-25 00:15:09 +0300

    feat: Added build.gradle.kts

--- mg-identity-service-access-token/build.gradle.kts
+++ mg-identity-service-access-token/build.gradle.kts
@@ -1,8 +1,5 @@
 dependencies {
 
     implementation(project(":mg-identity-service-common"))
-
     implementation(project(":mg-identity-service-token"))
-    implementation(project(":mg-identity-service-user"))
-    implementation(project(":mg-identity-service-principal"))
 }
