2ebbf9add55b618412aa686b119e641e8cffa07b
Святослав <slava.minasyan2006@mail.ru>
2025-08-05 14:05:40 +0300

    feat: Minor changes

--- mg-identity-service-user/src/main/java/ru/solomka/identity/user/UserService.java
+++ mg-identity-service-user/src/main/java/ru/solomka/identity/user/UserService.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
 import ru.solomka.identity.common.EntityService;
+import ru.solomka.identity.common.exception.EntityNotFoundException;
 
 import java.util.Optional;
 
@@ -15,6 +16,26 @@ public class UserService extends EntityService<UserEntity> {
     public UserService(@NonNull UserRepository repository) {
         super(repository);
         this.userRepository = repository;
+    }
+
+    public UserEntity getByLogin(@NonNull String login){
+        return this.findByLogin(login)
+                .orElseThrow(() -> new EntityNotFoundException("User with login '%s' not found".formatted(login)));
+    }
+
+    public UserEntity getByEmail(@NonNull String email){
+        return this.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("User with email '%s' not found".formatted(email)));
+    }
+
+    public UserEntity getByFirstName(@NonNull String firstName){
+        return this.findByFirstName(firstName)
+                .orElseThrow(() -> new EntityNotFoundException("User with name '%s' not found".formatted(firstName)));
+    }
+
+    public UserEntity getByLastName(@NonNull String lastName){
+        return this.findByFirstName(lastName)
+                .orElseThrow(() -> new EntityNotFoundException("User with lastname '%s' not found".formatted(lastName)));
     }
 
     public Optional<UserEntity> findByLogin(@NonNull String login) {
