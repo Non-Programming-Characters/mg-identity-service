79ffdc934793054a8064d32f5e397695f595d6b3
Святослав <slava.minasyan2006@mail.ru>
2025-08-05 14:05:30 +0300

    feat: Added CORS configuration bean

--- mg-identity-service-spring/src/main/java/ru/solomka/identity/spring/configuration/security/HttpSecurityConfiguration.java
+++ mg-identity-service-spring/src/main/java/ru/solomka/identity/spring/configuration/security/HttpSecurityConfiguration.java
@@ -12,7 +12,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import ru.solomka.identity.spring.configuration.security.filter.OnceRequestFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -20,18 +25,31 @@ import ru.solomka.identity.spring.configuration.security.filter.OnceRequestFilte
 public class HttpSecurityConfiguration {
 
     @Bean
-    SecurityFilterChain securityFilterChain(@NotNull HttpSecurity http, OnceRequestFilter requestFilter) throws Exception {
+    SecurityFilterChain securityFilterChain(@NotNull HttpSecurity http, CorsConfigurationSource corsConfigurationSource, OnceRequestFilter requestFilter) throws Exception {
         return http
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(registry -> registry
                         .requestMatchers("/v1/security/auth/**", "/identity-mg/v3/api-docs/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(List.of("Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
