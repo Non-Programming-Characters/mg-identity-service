4efc9ffc61ee15102a14f73b9519bf29f4b646e0
Святослав <slava.minasyan2006@mail.ru>
2025-09-28 22:42:03 +0300

    feat: Notification kafka wrapper system

---
+++ mg-identity-service-kafka-adapter/src/main/java/ru/solomka/identity/kafka/KafkaTopicPoints.java
@@ -0,0 +1,7 @@
+package ru.solomka.identity.kafka;
+
+public class KafkaTopicPoints {
+    public static final String CREATE_EVENT_TOPIC = "mg-identity-service:create-user-event-topic";
+    public static final String UPDATE_EVENT_TOPIC = "mg-identity-service:update-user-event-topic";
+    public static final String DELETE_EVENT_TOPIC = "mg-identity-service:delete-user-event-topic";
+}
---
+++ mg-identity-service-kafka-adapter/src/main/java/ru/solomka/identity/kafka/event/KafkaUserDeletedEvent.java
@@ -0,0 +1,17 @@
+package ru.solomka.identity.kafka.event;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import ru.solomka.identity.principal.PrincipalEntity;
+import ru.solomka.identity.user.UserEntity;
+
+@Getter
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class KafkaUserDeletedEvent extends KafkaEvent<UserEntity, PrincipalEntity> {
+    @NonNull UserEntity firstChapterNotifyMessage;
+    @NonNull PrincipalEntity secondChapterNotifyMessage;
+}
