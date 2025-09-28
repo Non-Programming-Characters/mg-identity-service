badd3e798a59a8d5f4098aff7fde8e813abf9b8d
Святослав <slava.minasyan2006@mail.ru>
2025-09-28 22:54:00 +0300

    feat: Added kafka notifications after any operations with user entity, unnecessary methods removed

--- mg-identity-service-user/src/main/java/ru/solomka/identity/user/UserService.java
+++ mg-identity-service-user/src/main/java/ru/solomka/identity/user/UserService.java
@@ -3,19 +3,47 @@ package ru.solomka.identity.user;
 import lombok.AccessLevel;
 import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
+import ru.solomka.identity.common.EntityNotificationService;
 import ru.solomka.identity.common.EntityService;
 import ru.solomka.identity.common.exception.EntityNotFoundException;
 
+import java.time.Instant;
 import java.util.Optional;
+import java.util.UUID;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserService extends EntityService<UserEntity> {
 
     @NonNull UserRepository userRepository;
+    @NonNull EntityNotificationService<UserEntity> notificationService;
 
-    public UserService(@NonNull UserRepository repository) {
+    public UserService(@NonNull UserRepository repository,
+                       @NonNull EntityNotificationService<UserEntity> notificationService) {
         super(repository);
         this.userRepository = repository;
+        this.notificationService = notificationService;
+    }
+
+    @Override
+    public UserEntity create(UserEntity entity) {
+        entity.setCreatedAt(Instant.now());
+        UserEntity createdUserEntity = super.create(entity);
+        notificationService.notifyCreated(createdUserEntity);
+        return createdUserEntity;
+    }
+
+    @Override
+    public UserEntity update(UserEntity entity) {
+        UserEntity updatedUserEntity = super.update(entity);
+        notificationService.notifyUpdated(updatedUserEntity);
+        return updatedUserEntity;
+    }
+
+    @Override
+    public UserEntity deleteById(UUID id) {
+        UserEntity deletedUserEntity = super.deleteById(id);
+        notificationService.notifyDeleted(deletedUserEntity);
+        return deletedUserEntity;
     }
 
     public UserEntity getByLogin(@NonNull String login){
@@ -28,29 +56,11 @@ public class UserService extends EntityService<UserEntity> {
                 .orElseThrow(() -> new EntityNotFoundException("User with email '%s' not found".formatted(email)));
     }
 
-    public UserEntity getByFirstName(@NonNull String firstName){
-        return this.findByFirstName(firstName)
-                .orElseThrow(() -> new EntityNotFoundException("User with name '%s' not found".formatted(firstName)));
-    }
-
-    public UserEntity getByLastName(@NonNull String lastName){
-        return this.findByFirstName(lastName)
-                .orElseThrow(() -> new EntityNotFoundException("User with lastname '%s' not found".formatted(lastName)));
-    }
-
     public Optional<UserEntity> findByLogin(@NonNull String login) {
         return userRepository.findByLogin(login);
     }
 
     public Optional<UserEntity> findByEmail(@NonNull String email) {
         return userRepository.findByEmail(email);
-    }
-
-    public Optional<UserEntity> findByFirstName(@NonNull String firstName) {
-        return userRepository.findByFirstName(firstName);
-    }
-
-    public Optional<UserEntity> findByLastName(@NonNull String lastName) {
-        return userRepository.findByLastName(lastName);
     }
 }
