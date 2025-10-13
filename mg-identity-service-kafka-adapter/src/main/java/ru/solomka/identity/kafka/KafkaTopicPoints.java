8fe6eb37343a4682869ba9747900ee4c581278c4
Святослав <slava.minasyan2006@mail.ru>
2025-10-13 21:33:44 +0300

    fix: Changed topics names to correct format

--- mg-identity-service-kafka-adapter/src/main/java/ru/solomka/identity/kafka/KafkaTopicPoints.java
+++ mg-identity-service-kafka-adapter/src/main/java/ru/solomka/identity/kafka/KafkaTopicPoints.java
@@ -1,7 +1,7 @@
 package ru.solomka.identity.kafka;
 
 public class KafkaTopicPoints {
-    public static final String CREATE_EVENT_TOPIC = "mg-identity-service:create-user-event-topic";
-    public static final String UPDATE_EVENT_TOPIC = "mg-identity-service:update-user-event-topic";
-    public static final String DELETE_EVENT_TOPIC = "mg-identity-service:delete-user-event-topic";
+    public static final String CREATE_EVENT_TOPIC = "mg-identity-service_create-user-event-topic";
+    public static final String UPDATE_EVENT_TOPIC = "mg-identity-service_update-user-event-topic";
+    public static final String DELETE_EVENT_TOPIC = "mg-identity-service_delete-user-event-topic";
 }
