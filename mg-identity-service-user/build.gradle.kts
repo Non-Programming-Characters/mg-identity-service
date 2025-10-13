771e54e837f156d7b41a2304fed6a580e0948c83
Святослав <slava.minasyan2006@mail.ru>
2025-10-13 21:29:36 +0300

    feat: Added Jackson dependencies

--- mg-identity-service-user/build.gradle.kts
+++ mg-identity-service-user/build.gradle.kts
@@ -1,4 +1,7 @@
 dependencies {
     implementation(project(":mg-identity-service-common"))
     implementation(project(":mg-identity-service-principal"))
+
+    implementation(rootProject.libs.jackson.core.core)
+    implementation(rootProject.libs.jackson.core.annotations)
 }
