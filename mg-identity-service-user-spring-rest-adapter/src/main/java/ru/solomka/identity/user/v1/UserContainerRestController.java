324e54c77b4522f6599764da85ff571afd543723
Святослав <slava.minasyan2006@mail.ru>
2025-10-13 21:40:13 +0300

    feat: Changed structure

--- mg-identity-service-user-spring-rest-adapter/src/main/java/ru/solomka/identity/user/v1/UserContainerRestController.java
+++ mg-identity-service-user-spring-rest-adapter/src/main/java/ru/solomka/identity/user/v1/UserContainerRestController.java
@@ -12,24 +12,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.solomka.identity.common.cqrs.CommandHandler;
 import ru.solomka.identity.user.UserEntity;
-import ru.solomka.identity.user.UserSearchResponseUserEntityMapper;
+import ru.solomka.identity.user.cqrs.query.GetUserByEmailQuery;
 import ru.solomka.identity.user.cqrs.query.GetUserByIdQuery;
-import ru.solomka.identity.user.response.UserSearchResponse;
+import ru.solomka.identity.user.cqrs.query.GetUserByLoginQuery;
 
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/v1/api/security/user")
+@RequestMapping("/v1/api/identity/container/search")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserContainerRestController {
 
     @NonNull CommandHandler<GetUserByIdQuery, UserEntity> getUserByIdQueryHandler;
-    @NonNull UserSearchResponseUserEntityMapper userSearchResponseUserEntityMapper;
+    @NonNull CommandHandler<GetUserByLoginQuery, UserEntity> getUserByLoginQueryHandler;
+    @NonNull CommandHandler<GetUserByEmailQuery, UserEntity> getUserByEmailQueryHandler;
 
-    @GetMapping(produces = "application/json")
-    public ResponseEntity<UserSearchResponse> getUserById(@RequestParam("id") UUID id) {
-        UserEntity userEntity = getUserByIdQueryHandler.handle(new GetUserByIdQuery(id));
-        return ResponseEntity.ok(userSearchResponseUserEntityMapper.mapToInfrastructure(userEntity));
+    @GetMapping(value ="/spec", produces = "application/json")
+    public ResponseEntity<UserEntity> getUserById(@RequestParam("searchBy") String searchBy, @RequestParam("value") Object value) {
+
+        switch (searchBy) {
+            case "email" -> {
+                UserEntity userEntity = getUserByEmailQueryHandler.handle(new GetUserByEmailQuery(String.valueOf(value)));
+                return ResponseEntity.ok(userEntity);
+            }
+            case "login" -> {
+                UserEntity userEntity = getUserByLoginQueryHandler.handle(new GetUserByLoginQuery(String.valueOf(value)));
+                return ResponseEntity.ok(userEntity);
+            }
+            case "id" -> {
+                UserEntity userEntity = getUserByIdQueryHandler.handle(new GetUserByIdQuery(UUID.fromString(String.valueOf(value))));
+                return ResponseEntity.ok(userEntity);
+            }
+            default -> throw new IllegalArgumentException("Invalid search parameter");
+        }
     }
 }
